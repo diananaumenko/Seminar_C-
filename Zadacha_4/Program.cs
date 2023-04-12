27a45217bba122c8ad00e2fd4349542f81eb54d1
Diana Naumenko <diananaymenko@mail.ru>
2023-04-13 00:27:50 +0300

    Zadacha_4

---
+++ Zadacha_4/Program.cs
@@ -0,0 +1,40 @@
+﻿// Напишите программу, которая принимает на вход 
+// три числа и выдает максимальное из этих чисел
+// 2, 3, 7 -> 7;
+// 44, 5, 78 -> 78;
+// 22, 3, 9 -> 22
+
+Console.Write("Введите A:");
+string text1 = Console.ReadLine();
+int number1 = Convert.ToInt32(text1);
+Console.Write("Введите B:");
+string text2 = Console.ReadLine();
+int number2 = Convert.ToInt32(text2);
+Console.Write("Введите С:");
+string text3 = Console.ReadLine();
+int number3 = Convert.ToInt32(text3);
+
+if (number1 < number2)
+{
+    Console.WriteLine("max = В");
+}
+else
+{
+    Console.WriteLine("max = A");
+}
+if (number2 < number3)
+{
+    Console.WriteLine("max = С");
+}
+else
+{
+    Console.WriteLine("max = B");
+}
+if (number1 < number3)
+{
+    Console.WriteLine("max = C");
+}
+else
+{
+    Console.WriteLine("max = A");
+}
