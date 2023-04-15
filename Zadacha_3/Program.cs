4dd379b8676ca349d91cf9ec5f53f0862a7f9379
Diana Naumenko <diananaymenko@mail.ru>
2023-04-15 10:26:24 +0300

    Zadacha_3

--- Zadacha_3/Program.cs
+++ Zadacha_3/Program.cs
@@ -2,35 +2,70 @@
 // 3 -> среда
 // 5 -> пятница
 
+// Console.Write("Введите номер дня недели: ");
+// string text = Console.ReadLine();
+// int numberDayOfWeek = Convert.ToInt32(text);
+
+// if (numberDayOfWeek == 1)
+// {
+//     Console.WriteLine("Понидельник");
+// }
+// if (numberDayOfWeek == 2)
+// {
+//     Console.WriteLine("Вторник");
+// }
+// if (numberDayOfWeek == 3)
+// {
+//     Console.WriteLine("Среда");
+// }
+// if (numberDayOfWeek == 4)
+// {
+//    Console.WriteLine("Четверг");
+// }
+// if (numberDayOfWeek == 5)
+// {
+//     Console.WriteLine("Пятница");
+// }
+// if (numberDayOfWeek == 6)
+// {
+//     Console.WriteLine("Суббота");
+// }
+// if (numberDayOfWeek ==7)
+// {
+//     Console.WriteLine("Воскресенье");
+// }
+
+// альтернативное решение
+
 Console.Write("Введите номер дня недели: ");
 string text = Console.ReadLine();
 int numberDayOfWeek = Convert.ToInt32(text);
 
 if (numberDayOfWeek == 1)
 {
-    Console.WriteLine("Понидельник");
+    Console.WriteLine("Понедельник");
 }
-if (numberDayOfWeek == 2)
+else if (numberDayOfWeek ==2)
 {
     Console.WriteLine("Вторник");
 }
-if (numberDayOfWeek == 3)
+else if (numberDayOfWeek == 3)
 {
     Console.WriteLine("Среда");
 }
-if (numberDayOfWeek == 4)
+else if (numberDayOfWeek == 4)
 {
     Console.WriteLine("Четверг");
 }
-if (numberDayOfWeek == 5)
+else if (numberDayOfWeek == 5)
 {
     Console.WriteLine("Пятница");
 }
-if (numberDayOfWeek == 6)
+else if (numberDayOfWeek ==6)
 {
     Console.WriteLine("Суббота");
 }
-if (numberDayOfWeek ==7)
+else if (numberDayOfWeek == 7)
 {
-    Console.WriteLine("Вовкресенье");
+    Console.WriteLine("Воскресенье");
 }
