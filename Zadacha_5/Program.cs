0f68cb3120b4945ad7f8d8526da9738776a96840
Diana Naumenko <diananaymenko@mail.ru>
2023-04-15 10:43:43 +0300

    Zadacha_5

---
+++ Zadacha_5/Program.cs
@@ -0,0 +1,22 @@
+﻿// Напишите программу, которая на вход принимает одно число (N),
+// а на выходе показывает все целые числа в промежутке от - N до N.
+// 4 -> "-4, -3, -2, -1, 0, 1, 2, 3, 4"
+// 2 -> "-2, -1, 0, 1, 2"
+
+Console.WriteLine("Введите число: ");
+string text = Console.ReadLine();
+int number = Convert.ToInt32(text);
+int negativeNumber = number *-1;
+if (number > 0) 
+{
+while (negativeNumber <= number)
+{
+    Console.Write($"{negativeNumber} ");
+    Console.Write("");
+    negativeNumber++;
+}
+}
+else
+{
+    Console.WriteLine("Число должно быть положительным");
+}
